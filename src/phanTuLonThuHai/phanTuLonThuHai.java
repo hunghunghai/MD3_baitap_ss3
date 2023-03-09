23d85fb57c192d3f1d795c05cda0c772f7bfd3eb
Nguyễn Phi Hùng <phuongpidz@gmail.com>
2023-03-09 21:48:51 +0700

    bài tập

---
+++ src/phanTuLonThuHai/phanTuLonThuHai.java
@@ -0,0 +1,21 @@
+package phanTuLonThuHai;
+
+public class phanTuLonThuHai {
+    public static void main(String[] args) {
+        int[] arr = {81, 56, 8, 999, 100, 101, 99};
+        int max = arr[0];
+        int max2 = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            int block;
+            if (max2 < arr[i]) {
+                max2 = arr[i];
+                if (max2 > max) {
+                    block = max;
+                    max = max2;
+                    max2 = block;
+                }
+            }
+        }
+        System.out.println("gia tri lon t2 la: " + max2);
+    }
+}
---
+++ src/timPhanTuMatranLonNhat/PhantuLonNhat.java
@@ -0,0 +1,31 @@
+package timPhanTuMatranLonNhat;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PhantuLonNhat {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("nhap chieu dai mang: ");
+        int n = input.nextInt();
+        System.out.print("nhap chieu rong mang: ");
+        int m = input.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("nhap phan tu cho mang " + "[" + i + "," + j + "]: ");
+                arr[i][j] = input.nextInt();
+            }
+        }
+        System.out.println(Arrays.deepToString(arr));
+        int max = arr[0][0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.println("gia tri lon nhat la: " + max);
+    }
+}
