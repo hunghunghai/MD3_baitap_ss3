ac9f8e0def4abc9826b790511a194748fa14f638
Nguyễn Phi Hùng <phuongpidz@gmail.com>
2023-03-10 08:42:09 +0700

    bổ sung

--- src/demLanXuatHien/demLanXuatHien.java
+++ src/demLanXuatHien/demLanXuatHien.java
@@ -1,7 +1,25 @@
 package demLanXuatHien;
 
+import java.util.Scanner;
+
 public class demLanXuatHien {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        // Bước 1: Khai báo và nhập chuỗi
+        System.out.print("Nhập chuỗi: ");
+        String str = scanner.nextLine();
+        // Bước 2: Khai báo và nhập ký tự cần đếm
+        System.out.print("Nhập ký tự cần đếm: ");
+        char c = scanner.nextLine().charAt(0);
+        // Bước 3: Khai báo biến đếm
+        int count = 0;
+        // Bước 4: Sử dụng vòng lặp để duyệt từng ký tự trong chuỗi và đếm số ký tự cần đếm
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        // Bước 5: In ra kết quả
+        System.out.println("Số lần xuất hiện của ký tự '" + c + "' trong chuỗi là " + count);
     }
 }
--- src/phanTuLonThuHai/phanTuLonThuHai.java
+++ src/phanTuLonThuHai/phanTuLonThuHai.java
@@ -2,7 +2,7 @@ package phanTuLonThuHai;
 
 public class phanTuLonThuHai {
     public static void main(String[] args) {
-        int[] arr = {81, 56, 8, 999, 100, 101, 99};
+        int[] arr = {81, 56, 8, 999, 100, 101, 8};
         int max = arr[0];
         int max2 = arr[0];
         for (int i = 0; i < arr.length; i++) {
